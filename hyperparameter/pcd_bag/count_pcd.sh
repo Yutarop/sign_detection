83038a612a0868c2552b894c7f6b9d56b15b40b0
Yutarop <katoyutaro1122@icloud.com>
2025-06-29 04:02:30 +0000

    delete hyperparameter files

--- hyperparameter/pcd_bag/count_pcd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIRECTORY=~/ros2_ws/src/sign_detection/hyperparameter/pcd_bag
-
-count=$(ls "$DIRECTORY"/*.pcd 2>/dev/null | wc -l)
-
-echo "The number of pcd files in the directory: $count"
